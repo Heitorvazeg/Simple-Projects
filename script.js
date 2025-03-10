0f7972a269739c35a67a71ffc0d75bee4a372e41
Heitor Vaz <heitorvazeg@gmail.com>
2025-03-10 12:53:06 -0300

    Lógica que facilita o manuseio

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const Tela = document.getElementById('display');
 let resultadoamostra = false;
 
+function BotarOperadorNoDisplay(input) {
+    if (Tela.value == 'Error') {
+        Tela.value = input;
+    }
+    else {
+        Tela.value += input;
+        resultadoamostra = false;
+    };
+};
+
 function BotarNoDisplay(input) {
     if (resultadoamostra) {
         Tela.value = input;
